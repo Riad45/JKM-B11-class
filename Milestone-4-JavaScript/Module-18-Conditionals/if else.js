0b2652368e3374827db99ec511a9d17e438138a8
Riad45 <bingo45riad@gmail.com>
2025-02-10 00:24:23 +0600

    module 18 added

---
+++ Milestone-4-JavaScript/Module-18-Conditionals/if else.js
@@ -0,0 +1,11 @@
+let age = 16;
+let isAdultPersron = true;
+
+if(age>=15 && isAdultPersron == true){
+
+    console.log("You can go to the hunted house");
+}
+
+else{
+    console.log("You can't go there");
+}
