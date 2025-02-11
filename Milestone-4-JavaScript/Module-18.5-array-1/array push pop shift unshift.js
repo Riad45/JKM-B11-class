461850c88d9812f580e68bf776bafae13f76e657
Riad45 <bingo45riad@gmail.com>
2025-02-11 23:13:40 +0600

    Updated (18.5 module) Added( push pop shift and others)

---
+++ Milestone-4-JavaScript/Module-18.5-array-1/array push pop shift unshift.js
@@ -0,0 +1,18 @@
+let array = ["DIU", "UIU", "UAP", "NSU", "BRAC"]
+
+console.log(array);
+
+array.push("CITY","Manarat");
+console.log(array);
+
+array.pop();
+
+console.log(array);
+
+array.shift();
+
+console.log(array);
+
+array.unshift("DAFFODIL");
+
+console.log(array);
