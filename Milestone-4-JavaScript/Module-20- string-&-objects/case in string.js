14d8ed46f858aa7302da0170e399cc041f23eaa7
Riad45 <bingo45riad@gmail.com>
2025-02-12 23:57:05 +0600

    Updated(string task and practice)

---
+++ Milestone-4-JavaScript/Module-20- string-&-objects/case in string.js
@@ -0,0 +1,27 @@
+let string = "DHANMONDI_32"
+
+let string2= `lowercase to upper case`
+
+console.log(string);
+console.log(string2);
+
+
+console.log(string.toLowerCase());
+
+console.log(string2.toUpperCase());
+
+let string3 = "babuMoshai"
+
+let string4 = "  babuMoshai"
+
+if(string3.trim()=== string4.trim()){
+
+    console.log("ki khobor babu moshai");
+}
+
+else{
+
+    console.log("vondo babumoshai");
+}
+
+
