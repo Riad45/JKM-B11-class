aca537cfb00ffe63c21f65f6fe556bb5b1b957bf
Riad45 <bingo45riad@gmail.com>
2025-02-12 20:08:30 +0600

    added (loop task)

---
+++ Milestone-4-JavaScript/Module-19-loop/loop intro.js
@@ -0,0 +1,25 @@
+
+console.log("While loop");
+const array = [20,12,3,4,76,44,55,2];
+
+console.log(array);
+
+let i=0;
+
+while(i<=array.length-1){
+
+    console.log(array[i]);
+    i++;
+}
+
+
+console.log("for loop");
+
+const universities = ['Diu', "UIU","UAP","NSU","BracU","EWU"];
+
+console.log(universities);
+
+for(const university of universities){
+
+    console.log(university);
+} 
---
+++ Milestone-4-JavaScript/Module-19-loop/while loop task odd-even.js
@@ -0,0 +1,35 @@
+let num= 61;
+
+console.log("All the odd number from 61 to 100: ");
+
+while( num<=100 ){
+
+    if(num%2 === 0){
+
+        console.log(num);
+
+    }
+
+    num++;
+
+
+
+}
+
+let evenNum= 78;
+
+console.log("All the Even number from 78 to 98: ");
+
+while( evenNum<=98 ){
+
+    if(evenNum%2 === 0){
+
+        console.log(evenNum);
+
+    }
+
+    evenNum++;
+
+
+
+}
