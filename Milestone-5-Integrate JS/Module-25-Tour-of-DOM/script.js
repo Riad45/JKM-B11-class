fba8570c943d5a1bd999ce2bea155edf3b3744e9
Riad45 <bingo45riad@gmail.com>
2025-02-28 00:50:09 +0600

    added(Various ways of handiling event

--- Milestone-5-Integrate JS/Module-25-Tour-of-DOM/script.js
+++ Milestone-5-Integrate JS/Module-25-Tour-of-DOM/script.js
@@ -26,3 +26,20 @@ for(let selectItem of selectorAll){
 
     console.log(selectItem.innerText);
 }
+
+
+// dynamic style get set attribute classlist add remove innerText innerHTML
+const manipulation1 = document.getElementById("dynamic-p");
+
+// add style
+manipulation1.style.textAlign= "center";
+
+manipulation1.setAttribute("class","bg");
+
+// setAttribute("class", "bg") will remove all previous classes and replace them with "bg".
+manipulation1.getAttribute("class");
+manipulation1.setAttribute("class"," bg-2 bg-3");
+manipulation1.getAttribute("class");
+
+
+
