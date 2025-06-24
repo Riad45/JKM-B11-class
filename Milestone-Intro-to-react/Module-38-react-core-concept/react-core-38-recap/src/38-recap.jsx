218476f121435ee54e01124dadd40397c5e22cae
Riad45 <bingo45riad@gmail.com>
2025-06-25 00:21:53 +0600

    recap 38

---
+++ Milestone-Intro-to-react/Module-38-react-core-concept/react-core-38-recap/src/38-recap.jsx
@@ -0,0 +1,17 @@
+
+
+
+export default function Welcome(){
+
+    return(
+        <>
+
+        <h1>Hello let's recap the module 38 </h1>
+        <p>this is a welcome component</p>
+        
+        </>
+
+    )
+}
+
+// export default Welcome;
---
+++ Milestone-Intro-to-react/Module-38-react-core-concept/react-core-38-recap/src/App.jsx
@@ -0,0 +1,30 @@
+ import Welcome from './38-recap'
+import reactLogo from './assets/react.svg'
+
+
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+
+        
+      </div>
+
+      <Welcome/>
+
+      <Welcome></Welcome>
+      
+      
+    </>
+  )
+}
+
+export default App;
