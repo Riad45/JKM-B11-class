b9ddc9a7c88128dd96a40d8df112a6f00e67c193
Riad45 <bingo45riad@gmail.com>
2025-07-19 19:53:55 +0600

    recap (props , distructuring )

--- Milestone-7-Intro-to-react/Module-38-react-core-concept/react-core-38-recap/src/38-recap.jsx
+++ Milestone-7-Intro-to-react/Module-38-react-core-concept/react-core-38-recap/src/38-recap.jsx
@@ -3,11 +3,15 @@
 
 export default function Welcome(){
 
+    const date = "18 july 2025";
+
     return(
         <>
 
         <h1>Hello let's recap the module 38 </h1>
         <p>this is a welcome component</p>
+
+        <p>Today is : {date}</p>
         
         </>
 
--- Milestone-7-Intro-to-react/Module-38-react-core-concept/react-core-38-recap/src/App.jsx
+++ Milestone-7-Intro-to-react/Module-38-react-core-concept/react-core-38-recap/src/App.jsx
@@ -1,26 +1,66 @@
+import reactLogo from "./assets/react.svg";
 
-import reactLogo from './assets/react.svg'
+import Welcome from "./38-recap";
 
-
-
-import './App.css'
+import "./App.css";
 
 function App() {
-  
-
   return (
     <>
       <div>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-
-        
       </div>
-      
-      
+
+      <Welcome />
+
+      <Devoloper name="Rofiq" tech="Java" />
+      <Devoloper name="Salam" tech ="Python" />
+
+      <Person></Person>
     </>
-  )
+  );
 }
 
 export default App;
+
+function Devoloper(props) {
+
+
+  return (
+    <>
+      <div className="devStyle">
+        <h3>Dev Details</h3>
+        <h4>Name: {props.name} </h4>
+        <h4>Tech: {props.tech} </h4>
+      </div>
+    </>
+  );
+}
+
+function Person() {
+  const personName = "Riad";
+
+  const personStyle = {
+    color: "blue",
+    borderRadius: "10px",
+    border: "3px solid green",
+    padding: "20px",
+    marginBottom: "10px",
+  };
+  return (
+
+    <div style={personStyle}>
+      <p >Hi my name is : {personName}</p>
+
+      <p style={{
+        color: 'red',
+        fontSize: '30px'
+      }}>hey there </p>
+    </div>
+  );
+}
+
+
+
