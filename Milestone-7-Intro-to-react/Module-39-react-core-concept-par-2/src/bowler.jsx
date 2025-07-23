5b40317c715488ec9b4d5167e83b25ac2ecd0a9a
Riad45 <bingo45riad@gmail.com>
2025-07-23 20:17:55 +0600

    Update functionality of bowler component

--- Milestone-7-Intro-to-react/Module-39-react-core-concept-par-2/src/bowler.jsx
+++ Milestone-7-Intro-to-react/Module-39-react-core-concept-par-2/src/bowler.jsx
@@ -1,44 +1,87 @@
-import { useState } from "react"
+import { useState } from "react";
 
-export default function Bowler(){
-
-      const countStyle = {
+export default function Bowler() {
+  const countStyle = {
     padding: "10px",
     border: "2px solid yellow",
     margin: " 10px",
   };
 
+  const [ball, setBall] = useState(6);
+  const [comment, setComment] = useState("");
+  const [wideCount, setWideCount] = useState(0);
+  const [NoBallCount, setNoBallCount] = useState(0);
+  const [runs, setRuns] = useState(0);
 
+  const handleValidBall = () => {
+    const newValidBall = ball - 1;
 
-    const [ball, setBall] = useState(6);
-    const [comment, setComment] = useState("");
-    const [wideCount , setWideCount] = useState(0);
+    setBall(newValidBall);
 
-    const handleBall=()=>{
+    setComment("good ball...");
 
-        const newBall = ball-1;
+    const runsArray = [runs+1,runs+2, runs+4,runs+0,runs+6];
 
-        setBall(newBall)
+    const totalRuns = runsArray[Math.floor(Math.random()* runsArray.length)];
 
-        setComment("good ball...");
-
-
+        {
+      if(totalRuns === runs+4){
+        setComment(".........FOUR............!!")
+      }
+    }
+        {
+      if(totalRuns === runs+6){
+        setComment("...........SIX............!!")
+      }
     }
 
-    const handleWideball = ()=>{
-        const totalWide = wideCount +1;
-        setWideCount(totalWide);
-        setComment("oh no .. you are giving wide...");
+    setRuns(totalRuns);
+
+
+  };
+
+  const handleWideball = () => {
+    const totalWide = wideCount + 1;
+    setWideCount(totalWide);
+
+    const totalRuns = runs+1;
+    setRuns(totalRuns);
+    setComment("..........WIDE........");
+  };
+  const handleNoball = () => {
+    const totalNoBall = NoBallCount + 1;
+    setNoBallCount(totalNoBall);
+    const totalRuns = runs+1;
+    setRuns(totalRuns);
+    setComment("................ NO ball.........");
+  };
+
+  const handleBall = () => {
+    const ballArray = [handleValidBall, handleWideball, handleNoball];
+
+    const randomball = ballArray[Math.floor(Math.random() * ballArray.length)];
+
+
+
+
+        {
+      if (ball === 0) {
+        return setBall(0), setComment("your over is finished");
+      }
     }
 
+   randomball();
+  };
 
-    return(
-        <div style={countStyle}>
-            <h1>Ball remaining : {ball}</h1>
-            <small>total wide : {wideCount}</small> <br /> <br />
-            <button onClick={handleBall}>Ball</button>
-            <button onClick={handleWideball}>Wide Ball</button>
-            <h3>Comment: {comment} </h3>
-        </div>
-    )
+  return (
+    <div style={countStyle}>
+      <h2>Ball remaining : {ball}</h2>
+      <h1>Total Runs you gave: {runs} </h1>
+      <small>total wide : {wideCount}</small> <br /> <br />
+      <small>total NoBall : {NoBallCount}</small> <br /> <br />
+      <button onClick={handleBall}>Ball</button>
+      {/* <button onClick={handleWideball}>Wide Ball</button> */}
+      <h3>Comment: {comment} </h3>
+    </div>
+  );
 }
