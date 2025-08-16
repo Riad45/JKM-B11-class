9e494823b619d1b6eaf4514047c0f744b31595c9
Riad45 <bingo45riad@gmail.com>
2025-08-16 23:35:46 +0600

    basic structure

--- Milestone-7-Intro-to-react/Module-42-Scribbles-Cafe/src/App.jsx
+++ Milestone-7-Intro-to-react/Module-42-Scribbles-Cafe/src/App.jsx
@@ -1,6 +1,8 @@
 
 import './App.css'
+import Blogs from './components/Blogs/Blogs'
 import NavBar from './components/navbar/NavBar'
+
 
 function App() {
   
@@ -8,6 +10,16 @@ function App() {
     <>
 
     <NavBar></NavBar>
+
+    
+
+
+    <div className="main-container flex gap-5">
+      <div className="blogs-container w-[70%] border-x-2  border-x-blue-600 px-5">
+        <Blogs></Blogs>
+      </div>
+      <div className="sideNote-container w-[30%] border-x-2  border-x-red-600 px-5">Side Note</div>
+    </div>
 
 
     </>
---
+++ Milestone-7-Intro-to-react/Module-42-Scribbles-Cafe/src/components/Blogs/Blogs.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+
+const Blogs = () => {
+
+    const [blogs ,setBlogs] = useState([]);
+
+useEffect(()=>{
+
+    fetch("blogs.json")
+    .then(res=>res.json())
+    .then(data=> setBlogs(data));
+
+    
+},[])
+
+console.log(blogs);
+
+    return (
+        <div>
+
+            <h2>Total Blogs: {blogs.length}</h2>
+            
+        </div>
+    );
+};
+
+export default Blogs;
