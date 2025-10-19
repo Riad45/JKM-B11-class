578dbe678def2391a41164ed4af8327d2249e3cf
Riad45 <bingo45riad@gmail.com>
2025-10-19 18:29:33 +0600

    project initialized

---
+++ Milestone-8-React-Router-state-management/Module-44-Tailwind-CSS-Axios-Rechart-Awesome-components/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import NavBar from './Component/NavBar/NavBar'
+
+function App() {
+  
+
+  return (
+    <>
+
+    <NavBar></NavBar>
+
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis vitae eligendi quo minus beatae officiis sint consequuntur laudantium iusto obcaecati.</p>
+
+    </>
+  )
+}
+
+export default App
