8a92b048916504a4881621d9421ec1f3b504597e
Riad45 <bingo45riad@gmail.com>
2025-10-19 21:02:28 +0600

    custom responsive  navbar created

--- Milestone-8-React-Router-state-management/Module-44-Tailwind-CSS-Axios-Rechart-Awesome-components/src/App.jsx
+++ Milestone-8-React-Router-state-management/Module-44-Tailwind-CSS-Axios-Rechart-Awesome-components/src/App.jsx
@@ -1,6 +1,7 @@
 
 import './App.css'
 import NavBar from './Component/NavBar/NavBar'
+import CustomNav from './CustomNav/CustomNav'
 
 function App() {
   
@@ -8,7 +9,7 @@ function App() {
   return (
     <>
 
-    <NavBar></NavBar>
+    <CustomNav></CustomNav>
 
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis vitae eligendi quo minus beatae officiis sint consequuntur laudantium iusto obcaecati.</p>
 
---
+++ Milestone-8-React-Router-state-management/Module-44-Tailwind-CSS-Axios-Rechart-Awesome-components/src/CustomNav/CustomNav.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import { Lia500Px } from "react-icons/lia";
+import Link from "./Link";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { MdCancel } from "react-icons/md";
+
+const navData = [
+  {
+    id: 1,
+    title: "Home",
+    url: "/",
+  },
+  {
+    id: 2,
+    title: "About",
+    url: "/about",
+  },
+  {
+    id: 3,
+    title: "Services",
+    url: "/services",
+  },
+  {
+    id: 4,
+    title: "Projects",
+    url: "/projects",
+  },
+  {
+    id: 5,
+    title: "Contact",
+    url: "/contact",
+  },
+];
+
+const CustomNav = () => {
+  const [open, setOpen] = useState(false);
+  const links = navData.map((route) => (
+    <Link route={route} key={route.id}></Link>
+  ));
+
+  return (
+    <nav className="flex justify-between p-6 ">
+
+      <ul className={`md:hidden absolute bg-amber-100 text-black 
+        ${open? 'top-20 left-10': 'top-20 -left-70'}  duration-1000 `}>{links}</ul>
+
+      <div className="flex justify-center items-center gap-5 ">
+        <button className="md:hidden" onClick={() => setOpen(!open)}>
+          {open ? <MdCancel size={35} /> : <GiHamburgerMenu size={35} />}
+        </button>
+        <h3>My Nav</h3>
+      </div>
+
+      <ul className="md:flex hidden gap-40 ">{links}</ul>
+
+      <button className="btn btn-outline">Sign In</button>
+    </nav>
+  );
+};
+
+export default CustomNav;
