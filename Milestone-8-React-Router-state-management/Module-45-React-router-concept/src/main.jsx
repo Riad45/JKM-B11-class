070a7c0e24c512ad544d8e200ae0a029fa25e985
Riad45 <bingo45riad@gmail.com>
2025-10-22 01:22:01 +0600

    routing using nav link

--- Milestone-8-React-Router-state-management/Module-45-React-router-concept/src/main.jsx
+++ Milestone-8-React-Router-state-management/Module-45-React-router-concept/src/main.jsx
@@ -4,16 +4,29 @@ import './index.css'
 import App from './App.jsx'
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
+import Home from './Components/Home/Home.jsx'
+import CustomNav from './Components/Header/CustomNav/CustomNav.jsx'
+import Services from './Components/Services/Services.jsx'
+import About from './Components/About/About.jsx';
+
 
 const router = createBrowserRouter([
   {
-    path : '/',
-    element : <div>Hey there staring today</div>
+    path: "/",
+    Component : CustomNav,
+    children :[
+      {index: true , Component: Home},
+      {path: "services" , Component : Services},
+      {path: "about" , Component : About}
+
+    ]
   }
 ])
 
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-   <RouterProvider router={router}></RouterProvider>
+    <RouterProvider router={router}></RouterProvider>
+   
   </StrictMode>,
 )
